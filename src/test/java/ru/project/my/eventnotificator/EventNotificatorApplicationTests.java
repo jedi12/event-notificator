00ab4eb3a5cec50c84dd2934c88ec6599a325fde
a.pribytkov <jedi12@mail.ru>
2026-02-26 10:19:52 +0700

    Init commit

---
+++ src/test/java/ru/project/my/eventnotificator/EventNotificatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.project.my.eventnotificator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventNotificatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
