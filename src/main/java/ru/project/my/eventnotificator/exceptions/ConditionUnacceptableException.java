2c015b60daae7fdbb5c88233838bf092d45a9e0a
a.pribytkov <jedi12@mail.ru>
2026-03-01 14:06:09 +0700

    Revert "Выпускной проект: итерация 4 (EventNotificator)"
    This reverts commit c33fae23396d87862439133efb9caf16adcf73fc.

--- src/main/java/ru/project/my/eventnotificator/exceptions/ConditionUnacceptableException.java
+++
@@ -1,8 +0,0 @@
-package ru.project.my.eventnotificator.exceptions;
-
-public class ConditionUnacceptableException extends RuntimeException {
-
-    public ConditionUnacceptableException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/project/my/eventnotificator/exceptions/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.project.my.eventnotificator.exceptions;
-
-public class NotFoundException extends RuntimeException {
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
